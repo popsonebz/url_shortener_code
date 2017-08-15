178f42f88c7725e749c6f04c7694f0f0be7f465e
popsonebz <popoolaebenezer@gmail.com>
2017-08-15 14:55:25 +0200

    using self and instance to prevent circular import

--- shortener/models.py
+++ shortener/models.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import random
-import string
+from .utils import code_generator, create_shortcode
 from django.db import models
 
 # Create your models here.
-def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
-	return ''.join(random.choice(chars) for _ in range(size))
 
 
 
 class KirrURL(models.Model):
 	url = models.CharField(max_length=220)
-	shortcode = models.CharField(max_length=15, unique=True)
+	shortcode = models.CharField(max_length=15, unique=True, blank=True)
 	updated = models.DateTimeField(auto_now=True) #everytime the model is saved
 	timestamp = models.DateTimeField(auto_now_add=True) #when the model was created
 	#empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False) #
@@ -21,5 +18,6 @@ class KirrURL(models.Model):
 		return str(self.url)
 
 	def save(self, *args, **kwargs): #overiding the default save
-		self.shortcode = code_generator()
+		if self.shortcode is None or self.shortcode == "":
+			self.shortcode = create_shortcode(self)
 		super(KirrURL, self).save(*args, **kwargs)
